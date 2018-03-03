ffb3db0c254de1e101a88f868d1b6ebdf11aea05
Namkai <namkai.fairfield@gmail.com>
2018-03-03 21:02:28 +0100

    created build_pixel_map function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,6 +8,22 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map(grid, fn {_code, index} ->
+        horizantal = rem(index, 5) * 50
+        verticle = div(index, 5) * 50
+
+        top_left = {horizantal, verticle}
+        bottom_right = {horizantal + 50, verticle + 50}
+
+        {top_left, bottom_right}
+      end)
+
+      %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
