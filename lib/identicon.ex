189886804c19fc9c82b080976bb7f350549adc73
Namkai <namkai.fairfield@gmail.com>
2018-03-03 20:26:14 +0100

    created build_grid method and helper mirror_row function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,8 +6,21 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
+  @doc """
+    Takes a string as an input and returnsa a Identicon.Image struct
+    with the hex property turned into a list of numbers.
+    Given the same input it will always return the same output.
+
+  ## Example
+      iex> Identicon.hash_input('bannana')
+      %Identicon.Image{
+      color: nil,
+      hex: [158, 197, 170, 37, 92, 55, 209, 212, 165, 137, 106, 155, 190, 51, 35, 58]
+      }
+  """
   def hash_input(input) do
     hex =
       :crypto.hash(:md5, input)
@@ -16,7 +29,23 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
+
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-    %Identicon.Image{image | color: {r,g,b} }
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def mirror_row([first, second |  _tail] = row) do
+    row ++ [second, first]
+  end
+
+  def build_grid(%Identicon.Image{hex: hex } = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
   end
 end
+
+
+
+
+
