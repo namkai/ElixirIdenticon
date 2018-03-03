5edd460045225b5dc6442ac6f619bcb9d8dbbf2e
Namkai <namkai.fairfield@gmail.com>
2018-03-03 19:49:35 +0100

    created pick_color method

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,7 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
 
   def hash_input(input) do
@@ -13,5 +14,11 @@ defmodule Identicon do
       |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+
+    %Identicon.Image{image | color: {r,g,b} }
   end
 end
